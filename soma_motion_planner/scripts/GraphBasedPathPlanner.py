273c375e32e0dc518f99c8b82f005f22bdf06220
Ayumu <tommy.tommy.walker@gmail.com>
2020-09-27 12:25:03 +0900

    Add DelaunayNetwork

--- soma_motion_planner/scripts/GraphBasedPathPlanner.py
+++ soma_motion_planner/scripts/GraphBasedPathPlanner.py
@@ -20,19 +20,55 @@ isShow = True
 
 class GraphBasedPathPlanner:
     def __init__(self,):
+        self.landmarks = []
 
+        self.fig = plt.figure(figsize=(8,8))
+        self.ax_G = plt.subplot(2,2,1)
+        # self.ax_G.set_aspect('equal')
 
-        self.fig = plt.figure(figsize=(4,4))
-        self.ax = plt.subplot(2,1,1)
-        self.ax.set_aspect('equal')
+    def DelaunayNetwork(self,):
+        pos = [(p[0],p[1]) for p in self.landmarks]
+
+        #Delaunay Triangle Deconposition
+        tri = Delaunay(pos)
+
+        #new Graph object
+        G = nx.Graph()
+
+        for t in tri.simplices: #triangle iteration
+            if  distance.euclidean(pos[1], pos[0]) >= 2.5:
+                # G.add_edge(t[0],t[1],weight=distance.euclidean(pos[1], pos[0]))
+                G.add_edge(t[0],t[1])
+            if distance.euclidean(pos[2], pos[1]) >= 2.5:
+                # G.add_edge(t[1],t[2],weight=distance.euclidean(pos[2], pos[1]))
+                G.add_edge(t[1],t[2])
+            if distance.euclidean(pos[0], pos[2]) >= 2.5:
+                # G.add_edge(t[2],t[0],weight=distance.euclidean(pos[0], pos[2]))
+                G.add_edge(t[2],t[0])
+
+        return G
 
     def planning(self, env):
+
         if len(env.Trees)<=2:
             print('Not enough landmarks')
             print('{} <= 2'.format(len(env.Trees)))
 
+        #recontain to [xi,yi] list
+        self.landmarks = [[ti[0],ti[1]] for ti in env.Trees]
+
+        print('I) Make Initial Graph')
+        G = self.DelaunayNetwork()  #Delaynay Network Graph
+
+        print('|V|={},|E|={}'.format(G.number_of_nodes(),G.number_of_edges()))
+        
+
+        print('II) Modify to Eulerian')
 
 
+
+        if isShow:
+            nx.draw_networkx(G, pos=self.landmarks, ax=self.ax_G)
 
 # module test
 if __name__=='__main__':
