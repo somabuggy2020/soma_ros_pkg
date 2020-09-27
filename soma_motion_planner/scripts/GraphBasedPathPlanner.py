a02e923a6e72cee4fae09ca0521118facaa56d5c
Ayumu <tommy.tommy.walker@gmail.com>
2020-09-27 20:07:08 +0900

    Add Matching

--- soma_motion_planner/scripts/GraphBasedPathPlanner.py
+++ soma_motion_planner/scripts/GraphBasedPathPlanner.py
@@ -24,6 +24,7 @@ class GraphBasedPathPlanner:
 
         self.fig = plt.figure(figsize=(8,8))
         self.ax_G = plt.subplot(2,2,1)
+        self.ax_Ge = plt.subplot(2,2,2)
         # self.ax_G.set_aspect('equal')
 
     def DelaunayNetwork(self,):
@@ -59,16 +60,35 @@ class GraphBasedPathPlanner:
 
         print('I) Make Initial Graph')
         G = self.DelaunayNetwork()  #Delaynay Network Graph
-
         print('|V|={},|E|={}'.format(G.number_of_nodes(),G.number_of_edges()))
         
 
         print('II) Modify to Eulerian')
+        print('II-i) Extraction odd nodes')
+        odds = [v for v in G if G.degree(v)%2==1] #extraction odd nodes
+        sg = nx.subgraph(G, odds) #odd nodes subgraph
+        print('|Vo|={}'.format(len(odds)))
 
+        print('II-ii) Compute additional edges')
+        #make complete graph using odds
+        Gc = nx.Graph()
+        for v in itertools.combinations(sg.nodes(), 2):
+            Gc.add_edge(v[0], v[1])
+        #maximal matching
+        M = nx.maximal_matching(Gc)
+        print('|M|:{}'.format(len(M)))
 
+        Ge = nx.MultiGraph()
+        Ge.add_edges_from(G.edges())
+        Ge.add_edges_from(M)
 
         if isShow:
-            nx.draw_networkx(G, pos=self.landmarks, ax=self.ax_G)
+            nx.draw_networkx(G, pos=self.landmarks, ax=self.ax_G, node_color='cyan')
+            nx.draw_networkx_nodes(sg, pos=self.landmarks, nodeList=odds, ax=self.ax_G, node_color='red')
+
+            nx.draw_networkx(Ge, pos=self.landmarks, ax=self.ax_Ge, node_color='cyan')
+            nx.draw_networkx_nodes(sg,pos=self.landmarks,nodeList=odds,ax=self.ax_Ge,node_color='red')
+            nx.draw_networkx_edges(Ge,pos=self.landmarks,edgelist=M,width=3.0,edge_color='red',ax=self.ax_Ge)
 
 # module test
 if __name__=='__main__':
