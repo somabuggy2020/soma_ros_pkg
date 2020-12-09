9015453faa34785b907224bb3a7dee0189965f17
SOMA NUC 1 <somabuggy2020@gmail.com>
2020-12-10 02:54:17 +0900

    fix neural_network

--- soma_perception/scripts/neural_network.py
+++ soma_perception/scripts/neural_network.py
@@ -2,6 +2,7 @@
 
 import rospy
 import numpy as np
+import itertools
 import csv
 import gc
 
@@ -50,39 +51,103 @@ def setLandMarks():
 class Neural_Network():
   def __init__(self):
     self.LAND_MARKS = setLandMarks()
-    self.robot_cood = self.get_robot_cood()
-    #self.LAND_MARKS_ROTATE = self.rotate_cood()
+    self.rotate_cood()
+    self.get_nearTree()
+    #self.LAND_MARKS_ROTATE()
 
-    #print('len : ', len(self.LAND_MARKS_ROTATE[0][1]))
-    #print('robo_cood_x : ', self.LAND_MARKS_ROTATE[0][0])
-    #print('robo_cood_y : ', self.LAND_MARKS_ROTATE[1][0][0])
-    #print('robo_cood_z : ', self.LAND_MARKS_ROTATE[2])
-    #print('robo_cood_deg : ', self.LAND_MARKS_ROTATE[0][1][2])
-    print('robot_cood', self.robot_cood[1])
+    #print('len : ', len(self.LAND_MARKS_ROTATE[0]))
+    #print('robo_cood_x : ', self.LAND_MARKS_ROTATE[1])
     #self.write_csv()
-    print('finish')
 
-  def get_robot_cood(self):
-    ROBOT_COOD = []
-    for x in np.linspace(0, 35, 351):    #351
-      for y in np.linspace(0, 35, 351):
-        for deg in range(360):  #360
-          r = np.radians(deg)
-          ROBOT_COOD.append([x, y, r])
+
+  def rotate_cood(self):
+    #ROBOT_COOD = []
+    #TREE_COOD = []
+    #COORDINATE = []
+
+    q_set = []  #list for c-space samples (x,y,theta)
+    x_set = np.arange(0, 35, 17.5)
+    y_set = np.arange(0, 35, 35)
+    th_set = np.arange(0, 360, 360)
+    self.q_set = list(itertools.product(x_set, y_set, th_set))
+    # print(x_set, y_set, th_set)
+    #print(self.q_set[0])
+
+    self.R_set = []
+    for x,y,th in self.q_set:
+      r = np.radians(th)
+      C = np.cos(r)
+      S = np.sin(r)
+      R_x = np.array([
+                      [C, -S, x],
+                      [S, C, y],
+                      [0, 0, 1]
+                      ])
+      # print(R_x)
+      self.R_set.append(R_x)
     
-    return ROBOT_COOD
+    #print('R_set:{}'.format(len(R_set)))
+    return 
 
-  #def rotate_cood(self):
-  #  ROBOT_COOD = []
-  #  TREE_COOD = []
-  #  LAND_MARKS_rotate = []
-    #LAND_MARKS_rotate = [ROBOT_COOD, TREE_COOD]
+  def get_nearTree(self):
+    NearTree = []
+    #nearTree = []
+    for n in range(len(self.q_set)):
+      num = 0
+      nearTree = []
+      for m in range(len(self.LAND_MARKS)):
+        #print('q_set : ', self.q_set[n][0])
+        #x = np.array([self.q_set[n][0], self.LAND_MARKS[m][0]])
+        #y = np.array([self.q_set[n][1], self.LAND_MARKS[m][1]])
+        x = np.array([self.q_set[n][0], self.q_set[n][1]])
+        y = np.array([self.LAND_MARKS[m][0], self.LAND_MARKS[m][1]])
+        distance = np.linalg.norm(x - y)
+        if num <= 2:
+          nearTree.append(np.append(self.LAND_MARKS[m], distance))
+          #nearTree.append([self.LAND_MARKS[m], distance])
+          #print('nearTree1 : ', nearTree)
+          nearTree.sort(key=lambda x: x[3])
+          #sort_nearTree = sorted(nearTree, key=lambda x: x[3])
+        elif distance <= nearTree[n][3]:
+          nearTree[n] = np.append(self.LAND_MARKS[m], distance)
+          nearTree.sort(key=lambda x: x[3])
 
-    #for x in np.linspace(0, 35, 36):    #351
-    #  for y in np.linspace(0, 35, 36):
-    #    for deg in range(360):  #360
+        #print('nearTree1 : ', sort_nearTree[n])
+
+        num += 1
+      NearTree.append(nearTree)
+      #NearTree.append(sort_nearTree)
+      #del nearTree
+
+    #print('nearTreelen : ', len(NearTree))
+    print('nearTree1 : ', NearTree)
+    #print('nearTree1 : ', NearTree[0][0])
+    #print('nearTree2 : ', NearTree[0][1])
+    #print('nearTree3 : ', NearTree[0][2])
+  
+
+  def LAND_MARKS_ROTATE(self):
+    self.TREE_COOD = []
+    COORDINATE_ALL = []
+    COORDINATE = []
+    coordinate = []
+    for a in range(len(self.R_set)):
+      #print('R_set : ', self.R_set[a])
+      for b in range(len(self.LAND_MARKS)):
+        #print('LAND : ', self.LAND_MARKS[b])
+        coordinate = np.dot(self.R_set[a], self.LAND_MARKS[b])
+        COORDINATE.append(coordinate)
+      COORDINATE_ALL.append(COORDINATE)
+    self.TREE_COOD.append(COORDINATE_ALL)
+    #print('coord : ', self.TREE_COOD[0][0])
+    return
+
+    #for x in np.linspace(0, 35, 2):    #351
+    #  for y in np.linspace(0, 35, 2):
+    #    for deg in range(2):  #360
 #
     #      r = np.radians(deg)
+    #      r = np.round(r,3)
     #      C = np.cos(r)
     #      S = np.sin(r)
     #      R_x = np.array([
@@ -91,21 +156,27 @@ class Neural_Network():
     #                     [0, 0, 1]
     #                     ])
     #      ROBOT_COOD.append([x, y, deg])
+#
     #      for i in range(len(self.LAND_MARKS)):
     #        coordinate = np.dot(R_x, self.LAND_MARKS[i])
-    #        TREE_COOD.append(coordinate)
+    #        COORDINATE.append([coordinate])
+    #        #COORDINATE.append([np.dot(R_x,self.LAND_MARKS[i])])
+    #      TREE_COOD.append(COORDINATE)
 #
-    #      LAND_MARKS_rotate.append([ROBOT_COOD, TREE_COOD])
+    #      #for i in self.LAND_MARKS:
+    #      #  coordinate = np.dot(R_x, i)
+    #      #  COORDINATE.append([np.dot(R_x, i)])
+    #      #TREE_COOD.append(COORDINATE)
 #
-    #return LAND_MARKS_rotate
+    #return ROBOT_COOD, TREE_COOD
   
 
   def write_csv(self):
     with open('/home/soma1/Documents/noboru/csv/dataset.csv', 'w') as file:
       writer = csv.writer(file)
       writer.writerow(['x', 'y', 'theta'])
-      for a in range(len(self.LAND_MARKS_ROTATE[0][0])):
-        writer.writerow([self.LAND_MARKS_ROTATE[0][0][a][0], self.LAND_MARKS_ROTATE[0][0][a][1], self.LAND_MARKS_ROTATE[0][0][a][2]])
+      for a in range(len(self.q_set)):
+        writer.writerow([self.q_set[a][0], self.q_set[a][1], self.q_set[a][2]])
 
     return
 
