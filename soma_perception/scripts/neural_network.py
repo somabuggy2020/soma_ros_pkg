db2453c5b87ef9690a37e09cf727f7c77a689b04
SOMA NUC 1 <somabuggy2020@gmail.com>
2020-12-10 19:32:59 +0900

    fix neural_network

--- soma_perception/scripts/neural_network.py
+++ soma_perception/scripts/neural_network.py
@@ -53,22 +53,14 @@ class Neural_Network():
     self.LAND_MARKS = setLandMarks()
     self.rotate_cood()
     self.get_nearTree()
-    #self.LAND_MARKS_ROTATE()
-
-    #print('len : ', len(self.LAND_MARKS_ROTATE[0]))
-    #print('robo_cood_x : ', self.LAND_MARKS_ROTATE[1])
-    #self.write_csv()
+    self.write_csv()
 
 
   def rotate_cood(self):
-    #ROBOT_COOD = []
-    #TREE_COOD = []
-    #COORDINATE = []
-
     q_set = []  #list for c-space samples (x,y,theta)
-    x_set = np.arange(0, 35, 17.5)
-    y_set = np.arange(0, 35, 35)
-    th_set = np.arange(0, 360, 360)
+    x_set = np.arange(0, 35, 1)
+    y_set = np.arange(0, 35, 1)
+    th_set = np.arange(0, 360, 1)
     self.q_set = list(itertools.product(x_set, y_set, th_set))
     # print(x_set, y_set, th_set)
     #print(self.q_set[0])
@@ -90,93 +82,46 @@ class Neural_Network():
     return 
 
   def get_nearTree(self):
-    NearTree = []
+    self.NEARTREE = []
     #nearTree = []
     for n in range(len(self.q_set)):
       num = 0
       nearTree = []
       for m in range(len(self.LAND_MARKS)):
-        #print('q_set : ', self.q_set[n][0])
-        #x = np.array([self.q_set[n][0], self.LAND_MARKS[m][0]])
-        #y = np.array([self.q_set[n][1], self.LAND_MARKS[m][1]])
         x = np.array([self.q_set[n][0], self.q_set[n][1]])
         y = np.array([self.LAND_MARKS[m][0], self.LAND_MARKS[m][1]])
         distance = np.linalg.norm(x - y)
         if num <= 2:
           nearTree.append(np.append(self.LAND_MARKS[m], distance))
-          #nearTree.append([self.LAND_MARKS[m], distance])
           #print('nearTree1 : ', nearTree)
           nearTree.sort(key=lambda x: x[3])
-          #sort_nearTree = sorted(nearTree, key=lambda x: x[3])
-        elif distance <= nearTree[n][3]:
-          nearTree[n] = np.append(self.LAND_MARKS[m], distance)
+        elif distance <= nearTree[2][3]:
+          nearTree[2] = np.append(self.LAND_MARKS[m], distance)
           nearTree.sort(key=lambda x: x[3])
-
+          #print('nearTree: ', nearTree[2][3])
+          #print('nearTree: ', nearTree)
         #print('nearTree1 : ', sort_nearTree[n])
-
         num += 1
-      NearTree.append(nearTree)
-      #NearTree.append(sort_nearTree)
-      #del nearTree
-
+      self.NEARTREE.append(nearTree)
+      
     #print('nearTreelen : ', len(NearTree))
-    print('nearTree1 : ', NearTree)
+    #print('nearTree1 : ', self.NearTree)
     #print('nearTree1 : ', NearTree[0][0])
     #print('nearTree2 : ', NearTree[0][1])
     #print('nearTree3 : ', NearTree[0][2])
   
-
-  def LAND_MARKS_ROTATE(self):
-    self.TREE_COOD = []
-    COORDINATE_ALL = []
-    COORDINATE = []
-    coordinate = []
-    for a in range(len(self.R_set)):
-      #print('R_set : ', self.R_set[a])
-      for b in range(len(self.LAND_MARKS)):
-        #print('LAND : ', self.LAND_MARKS[b])
-        coordinate = np.dot(self.R_set[a], self.LAND_MARKS[b])
-        COORDINATE.append(coordinate)
-      COORDINATE_ALL.append(COORDINATE)
-    self.TREE_COOD.append(COORDINATE_ALL)
-    #print('coord : ', self.TREE_COOD[0][0])
-    return
-
-    #for x in np.linspace(0, 35, 2):    #351
-    #  for y in np.linspace(0, 35, 2):
-    #    for deg in range(2):  #360
-#
-    #      r = np.radians(deg)
-    #      r = np.round(r,3)
-    #      C = np.cos(r)
-    #      S = np.sin(r)
-    #      R_x = np.array([
-    #                     [C, -S, x],
-    #                     [S, C, y],
-    #                     [0, 0, 1]
-    #                     ])
-    #      ROBOT_COOD.append([x, y, deg])
-#
-    #      for i in range(len(self.LAND_MARKS)):
-    #        coordinate = np.dot(R_x, self.LAND_MARKS[i])
-    #        COORDINATE.append([coordinate])
-    #        #COORDINATE.append([np.dot(R_x,self.LAND_MARKS[i])])
-    #      TREE_COOD.append(COORDINATE)
-#
-    #      #for i in self.LAND_MARKS:
-    #      #  coordinate = np.dot(R_x, i)
-    #      #  COORDINATE.append([np.dot(R_x, i)])
-    #      #TREE_COOD.append(COORDINATE)
-#
-    #return ROBOT_COOD, TREE_COOD
-  
-
   def write_csv(self):
     with open('/home/soma1/Documents/noboru/csv/dataset.csv', 'w') as file:
       writer = csv.writer(file)
-      writer.writerow(['x', 'y', 'theta'])
+      writer.writerow(['x', 'y', 'theta',
+                       'tree1_x', 'tree1_y', 'tree1_d',
+                       'tree2_x', 'tree2_y', 'tree2_d',
+                       'tree3_x', 'tree3_y', 'tree3_d'])
       for a in range(len(self.q_set)):
-        writer.writerow([self.q_set[a][0], self.q_set[a][1], self.q_set[a][2]])
+        writer.writerow([self.q_set[a][0], self.q_set[a][1], self.q_set[a][2],
+                         self.NEARTREE[a][0][0], self.NEARTREE[a][0][1], self.NEARTREE[a][0][3],
+                         self.NEARTREE[a][1][0], self.NEARTREE[a][1][1], self.NEARTREE[a][1][3],
+                         self.NEARTREE[a][2][0], self.NEARTREE[a][2][1], self.NEARTREE[a][2][3]])
 
     return
 
