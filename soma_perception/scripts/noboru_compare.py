1789e80354f0610293fccf1dd245d6677259d76b
SOMA NUC 1 <somabuggy2020@gmail.com>
2021-01-13 19:59:49 +0900

    add noboru_compare

--- soma_perception/scripts/noboru_compare.py
+++ soma_perception/scripts/noboru_compare.py
@@ -12,6 +12,13 @@ from tensorflow.python.keras.models import load_model
 import matplotlib.pyplot as plt
 import math
 
+import rospy
+import rosparam
+
+from std_msgs.msg import Float32MultiArray
+import rospy
+
+
 def setLandMarks():
   #9
   LandMarkMap = np.array([
@@ -27,14 +34,19 @@ def setLandMarks():
                          ])
   return LandMarkMap
 
-def main():
+
+class compare():
+  def __init__(self):
+   rospy.Subscriber('tree_list', Float32MultiArray, self.call_back)
+
+  def call_back(self, cood):
     LAND_MARKS = setLandMarks()
     # テスト用データ
-    test_data = np.loadtxt('/home/soma1/Documents/noboru/csv/noboru_make_treelist_test.csv', delimiter=',', comments='#')
-    #x_test = test_data[:, 3:15]
-    x_test = np.array([test_data[3:15]])
-    #print('x_test : ', x_test)
-    
+    #test_data = np.loadtxt('/home/soma1/Documents/noboru/csv/noboru_make_treelist_test.csv', delimiter=',', comments='#')
+    #x_test = np.array([test_data[3:15]])
+    #x_test = cood.data
+    print("get_data")
+
     # モデル生成
     model_alpha = keras.models.load_model('/home/soma1/Documents/noboru/model/sigmoid_kyoku_NEWrange_trivec_9_epoch50_node14_alpha.h5', compile=False)
     model_beta = keras.models.load_model('/home/soma1/Documents/noboru/model/sigmoid_kyoku_NEWrange_trivec_9_epoch50_node14_beta.h5', compile=False)
@@ -42,17 +54,12 @@ def main():
 
     # モデルの検証・性能評価
     y_test_alpha = model_alpha.predict(x_test)
-    #y_test_alpha = np.round(y_test_alpha)
     y_test_beta = model_beta.predict(x_test)
-    #y_test_beta = np.round(y_test_beta)
     y_test_gamma = model_gamma.predict(x_test)
-    #y_test_gamma = np.round(y_test_gamma)
 
     print('a : ', y_test_alpha)
     print('b : ', y_test_beta)
     print('c : ', y_test_gamma)
-    
-
 
     #fix id
     first_a = 0
@@ -75,11 +82,11 @@ def main():
     print('a_id : ', a)
     print('b_id : ', b)
     print('c_id : ', c)
-      
+
     ab = np.sqrt((LAND_MARKS[a - 1][0] - LAND_MARKS[b - 1][0])** 2 + (LAND_MARKS[a - 1][1] - LAND_MARKS[b - 1][1])** 2)
     bc = np.sqrt((LAND_MARKS[b - 1][0] - LAND_MARKS[c - 1][0])** 2 + (LAND_MARKS[b - 1][1] - LAND_MARKS[c - 1][1])** 2)
     ca = np.sqrt((LAND_MARKS[c - 1][0] - LAND_MARKS[a - 1][0])** 2 + (LAND_MARKS[c - 1][1] - LAND_MARKS[a - 1][1])** 2)    
-    
+
     if ab > 12 and bc > 12:
       for m in range(9):
         if second_b < y_test_beta[0][m] and first_b != y_test_beta[0][m]:
@@ -99,51 +106,13 @@ def main():
     print('NEW b_id : ', b)
     print('NEW c_id : ', c)
 
-
-    #x_id = test_data[:, 15:18]
-    #x_id = np.array([test_data[15:18]])
-    #FLAT_LIST = []
-    #for x in range(len(x_id)):
-    #  a_id = 0
-    #  b_id = 0
-    #  c_id = 0
-    #  a_acc_max = 0
-    #  b_acc_max = 0
-    #  c_acc_max = 0
-    #  a_acc_second = 0
-    #  b_acc_second = 0
-    #  c_acc_second = 0
-    #  for a_pos in range(len(y_test_alpha[x])):
-    #    if y_test_alpha[x][a_pos] >= a_acc_max:
-    #      a_acc_second = a_acc_max
-    #      a_second_id = a_id
-    #      a_acc_max = y_test_alpha[x][a_pos]
-    #      a_id = a_pos + 1
-    #    if a_acc_max > y_test_alpha[x][a_pos] and a_acc_second < y_test_alpha[x][a_pos]:
-    #      a_acc_second = y_test_alpha[x][a_pos]
-    #      a_second_id = a_pos + 1
-    #  for b_pos in range(len(y_test_beta[x])):
-    #    if y_test_beta[x][b_pos] >= b_acc_max:
-    #      b_acc_second = b_acc_max
-    #      b_second_id = b_id
-    #      b_acc_max = y_test_beta[x][b_pos]
-    #      b_id = b_pos + 1
-    #    if b_acc_max > y_test_beta[x][b_pos] and b_acc_second < y_test_beta[x][b_pos]:
-    #      b_acc_second = y_test_beta[x][b_pos]
-    #      b_second_id = b_pos + 1
-    #  for c_pos in range(len(y_test_gamma[x])):
-    #    if y_test_gamma[x][c_pos] >= c_acc_max:
-    #      c_acc_second = c_acc_max
-    #      c_second_id = c_id
-    #      c_acc_max = y_test_gamma[x][c_pos]
-    #      c_id = c_pos + 1
-    #    if c_acc_max > y_test_gamma[x][c_pos] and c_acc_second < y_test_gamma[x][c_pos]:
-    #      c_acc_second = y_test_gamma[x][c_pos]
-    #      c_second_id = c_pos + 1
-    #  if x_id[x][0] != a_id or x_id[x][1] != b_id or x_id[x][2] != c_id:
-    #    FLAT_LIST.append([x_id[x][0], x_id[x][1], x_id[x][2], a_id, b_id, c_id, a_acc_max, b_acc_max, c_acc_max, a_second_id, b_second_id, c_second_id, a_acc_second, b_acc_second, c_acc_second])
-    #np.savetxt('/home/soma1/Documents/noboru/csv/9_trivec_kyoku_no_match.csv', FLAT_LIST, delimiter=',')
-
+    return
 
 if __name__ == '__main__':
-    main()
+    #main()
+
+    rospy.init_node('compare', anonymous=True)
+  
+    node = compare()
+
+    rospy.spin()
--- soma_perception/scripts/noboru_make_treelist.py
+++ soma_perception/scripts/noboru_make_treelist.py
@@ -5,33 +5,164 @@ import numpy as np
 import itertools
 import structure
 import gc
+import csv
 
 from geometry_msgs.msg import PoseArray
+from std_msgs.msg import Float32MultiArray
 
 
 class make_treelist():
   def __init__(self):
-    
-    self.list_2 = []
-    rospy.Subscriber('/bounding_F/centroid_pose_array', PoseArray, self.call_back_F)
-    rospy.Subscriber('/bounding_B/centroid_pose_array', PoseArray, self.call_back_B)
-    
-
-  def call_back_F(self, center_F):
-    self.list_1 = []
-    #print("0.x    ",center_F.poses[0].position.x)
-    #print("0.y    ",center_F.poses[0].position.y)
-    self.seq = center_F.header.seq
-    for n in range(len(center_F.poses)):
-      #print('num_F : ', n)
-      self.list_1.append([center_F.poses[n].position.x, center_F.poses[n].position.y])
-
+    self.old_tree_num = 0
+    rospy.Subscriber('/bounding/centroid_pose_array', PoseArray, self.call_back)
+    self.list_pub = rospy.Publisher("tree_list", Float32MultiArray, queue_size=10)
   
-  def call_back_B(self, center_B):
-    for m in range(len(center_B.poses)):
-      #print('num_B : ', m)
-      self.list_1.append([center_B.poses[m].position.x, center_B.poses[m].position.y])
-    print('list : ', self.list_1)
+  def call_back(self, center):
+    self.list = []
+
+    self.tree_num = len(center.poses)
+
+
+    if self.tree_num >= 3 and self.tree_num != self.old_tree_num:
+      self.old_tree_num = self.tree_num
+      for n in range(len(center.poses)):
+        self.list.append([center.poses[n].position.y * -1, center.poses[n].position.x])
+      
+    if self.list != []:
+      print('list : ', self.list)
+      self.rotate_cood()
+      self.get_nearTree()
+      self.make_triangle()
+      self.get_tri_vec()
+      self.convert_kyoku()
+      self.write_csv()
+  
+  def rotate_cood(self):
+    q_set = []  #list for c-space samples (x,y,theta)
+    x_set = np.arange(0, 1, 1)
+    y_set = np.arange(0, 1, 1)
+    th_set = np.arange(0, 360, 360)
+    self.q_set = list(itertools.product(x_set, y_set, th_set))
+
+    self.R_set1 = []
+    self.R_set2 = []
+    for x,y,th in self.q_set:
+      r = np.radians(th)
+      C = np.cos(r)
+      S = np.sin(r)
+      R_x1 = np.array([
+                      [1, 0, -x],
+                      [0, 1, -y],
+                      [0, 0, 1]
+                      ])
+      R_x2 = np.array([
+                      [C, -S, 0],
+                      [S, C, 0],
+                      [0, 0, 1]
+                      ])
+      self.R_set1.append(R_x1)
+      self.R_set2.append(R_x2)
+    return
+  
+  def get_nearTree(self):
+    self.NEARTREE = []
+    for n in range(len(self.q_set)):
+      num = 0
+      nearTree = []
+      for m in range(len(self.list)):
+        x = np.array([self.q_set[n][0], self.q_set[n][1]])
+        y = np.array([self.list[m][0], self.list[m][1]])
+        distance = np.linalg.norm(x - y)
+        if num <= 2:
+          nearTree.append(np.append(self.list[m], distance))
+          nearTree.sort(key=lambda x: x[2])
+        elif distance <= nearTree[2][3]:
+          nearTree[2] = np.append(self.list[m], distance)
+          nearTree.sort(key=lambda x: x[2])
+        num += 1
+      for a in range(len(nearTree)):
+        LAND_MARKS_ROLL1 = np.dot(self.R_set1[n], [nearTree[a][0], nearTree[a][1], 1])
+        LAND_MARKS_ROLL = np.dot(self.R_set2[n], [LAND_MARKS_ROLL1[0], LAND_MARKS_ROLL1[1], 1])
+        nearTree[a] = [LAND_MARKS_ROLL[0], LAND_MARKS_ROLL[1], nearTree[a][2]]
+      self.NEARTREE.append(nearTree)
+
+
+  def make_triangle(self):
+    for n in range(len(self.NEARTREE)):
+      abD = np.sqrt((self.NEARTREE[n][0][0] - self.NEARTREE[n][1][0]) **2 + (self.NEARTREE[n][0][1] - self.NEARTREE[n][1][1]) **2)
+      bcD = np.sqrt((self.NEARTREE[n][1][0] - self.NEARTREE[n][2][0]) **2 + (self.NEARTREE[n][1][1] - self.NEARTREE[n][2][1]) **2)
+      caD = np.sqrt((self.NEARTREE[n][2][0] - self.NEARTREE[n][0][0]) **2 + (self.NEARTREE[n][2][1] - self.NEARTREE[n][0][1]) **2)
+
+      if(bcD > caD):
+        if(bcD < abD):
+          self.NEARTREE[n][0], self.NEARTREE[n][2] = self.NEARTREE[n][2], self.NEARTREE[n][0]
+      elif(caD > abD):
+        self.NEARTREE[n][0], self.NEARTREE[n][1] = self.NEARTREE[n][1], self.NEARTREE[n][0]
+      else:
+        self.NEARTREE[n][0], self.NEARTREE[n][2] = self.NEARTREE[n][2], self.NEARTREE[n][0]
+
+      vec_ab = np.array([(self.NEARTREE[n][1][0] - self.NEARTREE[n][0][0]), (self.NEARTREE[n][1][1] - self.NEARTREE[n][0][1])])
+      vec_ac = np.array([(self.NEARTREE[n][2][0] - self.NEARTREE[n][0][0]), (self.NEARTREE[n][2][1] - self.NEARTREE[n][0][1])])
+      cross = np.cross(vec_ab, vec_ac)
+      if(cross > 0):
+          self.NEARTREE[n][1], self.NEARTREE[n][2] = self.NEARTREE[n][2], self.NEARTREE[n][1]
+
+  def get_tri_vec(self):
+    self.TRI_VEC = []
+    for n in range(len(self.NEARTREE)):
+      ab_x = self.NEARTREE[n][0][0] - self.NEARTREE[n][1][0]
+      ab_y = self.NEARTREE[n][0][1] - self.NEARTREE[n][1][1]
+      bc_x = self.NEARTREE[n][1][0] - self.NEARTREE[n][2][0]
+      bc_y = self.NEARTREE[n][1][1] - self.NEARTREE[n][2][1]
+      ca_x = self.NEARTREE[n][2][0] - self.NEARTREE[n][0][0]
+      ca_y = self.NEARTREE[n][2][1] - self.NEARTREE[n][0][1]
+      self.TRI_VEC.append([ab_x, ab_y, bc_x, bc_y, ca_x, ca_y])
+
+  def convert_kyoku(self):
+    for a in range(len(self.q_set)):
+      ab_x = self.TRI_VEC[a][0]
+      ab_y = self.TRI_VEC[a][1]
+      bc_x = self.TRI_VEC[a][2]
+      bc_y = self.TRI_VEC[a][3]
+      ca_x = self.TRI_VEC[a][4]
+      ca_y = self.TRI_VEC[a][5]
+      self.TRI_VEC[a][0] = np.sqrt(ab_x** 2 + ab_y** 2)
+      self.TRI_VEC[a][1] = np.arctan2(ab_y,ab_x)
+      self.TRI_VEC[a][2] = np.sqrt(bc_x** 2 + bc_y** 2)
+      self.TRI_VEC[a][3] = np.arctan2(bc_y,bc_x)
+      self.TRI_VEC[a][4] = np.sqrt(ca_x** 2 + ca_y** 2)
+      self.TRI_VEC[a][5] = np.arctan2(ca_y,ca_x)
+
+      for b in range(3):
+        x = self.NEARTREE[a][b][0]
+        y = self.NEARTREE[a][b][1]
+        self.NEARTREE[a][b][0] = np.sqrt(x** 2 + y** 2)
+        self.NEARTREE[a][b][1] = np.arctan2(y,x)
+  
+  def write_csv(self):
+    with open('/home/soma1/Documents/noboru/csv/noboru_make_treelist_test.csv', 'w') as file:
+      writer = csv.writer(file)
+      writer.writerow(['#x', 'y', 'theta',
+                       'a_r', 'a_theta', 
+                       'b_r', 'b_theta', 
+                       'c_r', 'c_theta',
+                       'ab_r', 'ab_theta', 'bc_r', 'bc_theta', 'ca_r', 'ca_theta'])
+      for a in range(len(self.q_set)):
+        writer.writerow([self.q_set[a][0], self.q_set[a][1], self.q_set[a][2],
+                         self.NEARTREE[a][0][0], self.NEARTREE[a][0][1], 
+                         self.NEARTREE[a][1][0], self.NEARTREE[a][1][1], 
+                         self.NEARTREE[a][2][0], self.NEARTREE[a][2][1],
+                         self.TRI_VEC[a][0], self.TRI_VEC[a][1], self.TRI_VEC[a][2], self.TRI_VEC[a][3], self.TRI_VEC[a][4], self.TRI_VEC[a][5]])
+
+        tree_data = Float32MultiArray()
+        tree_data.data = np.array([self.NEARTREE[a][0][0], self.NEARTREE[a][0][1], 
+                              self.NEARTREE[a][1][0], self.NEARTREE[a][1][1], 
+                              self.NEARTREE[a][2][0], self.NEARTREE[a][2][1],
+                              self.TRI_VEC[a][0], self.TRI_VEC[a][1], self.TRI_VEC[a][2], self.TRI_VEC[a][3], self.TRI_VEC[a][4], self.TRI_VEC[a][5]])
+
+        self.list_pub.publish(tree_data)
+
+    return
 
 
 if __name__ == '__main__':
