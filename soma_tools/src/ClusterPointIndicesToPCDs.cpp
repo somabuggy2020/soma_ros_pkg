2f2aeb991b64714f8ff404cda8c5ecfb6125d27a
Ayumu <tommy.tommy.walker@gmail.com>
2021-02-28 00:25:54 +0900

    add soma_tools pkg

---
+++ soma_tools/src/ClusterPointIndicesToPCDs.cpp
@@ -0,0 +1,33 @@
+#include <ros/ros.h>
+#include <nodelet/nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace soma_tools
+{
+  class ClusterPointIndicesToPCDs : public nodelet::Nodelet
+  {
+  public:
+    ClusterPointIndicesToPCDs();
+
+  private:
+    virtual void onInit();
+
+  private:
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh;
+  };
+
+  ClusterPointIndicesToPCDs::ClusterPointIndicesToPCDs()
+      : nh(getNodeHandle()), pnh(getPrivateNodeHandle())
+  {
+  }
+
+  //
+  //
+  //
+  void ClusterPointIndicesToPCDs::onInit()
+  {
+  }
+}
+
+PLUGINLIB_EXPORT_CLASS(soma_tools::ClusterPointIndicesToPCDs, nodelet::Nodelet)
